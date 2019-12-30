ccf54e3352120c67208df7424765b5e7a00f24b2
Utkarsh Bajpai <bajpai.utkarsh.28@gmail.com>
2019-12-30 22:06:27 +0530

    intial commit

---
+++ src/test/java/com/abhiyukti/cardapi/CardApi/CardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhiyukti.cardapi.CardApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
